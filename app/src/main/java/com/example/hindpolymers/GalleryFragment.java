580279eaa9655d5239e7cf16367be3dd61b00952
shilpa <shilpa.mahendriker@yahoo.com>
2019-04-06 16:13:34 -0400

    Added navigation drawer and fragments

---
+++ app/src/main/java/com/example/hindpolymers/GalleryFragment.java
@@ -0,0 +1,50 @@
+package com.example.hindpolymers;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link GalleryFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link GalleryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GalleryFragment extends Fragment {
+
+
+    public GalleryFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_gallery, container, false);
+        return rootView;
+    }
+
+
+
+
+
+
+
+}
